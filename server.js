2385248196530abe869163821787b90f2cd68f52
Dx-fantome-hack <dxfantome7@gmail.com>
2024-10-01 23:17:45 +0100

    Update and rename server.js to 00

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-const router = express();
-
-router.get('/', (req, res) => {
-    return res.status(200).json({ status: "working" });
-});
-
-module.exports = router;
